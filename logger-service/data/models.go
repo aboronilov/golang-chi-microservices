701427ed880b32eb0705e1c850edebf3e544b8e9
Anatoly <boronilov@gmail.com>
2024-04-30 08:24:35 +0200

    logger service CRUD added

--- logger-service/data/models.go
+++ logger-service/data/models.go
@@ -6,13 +6,14 @@ import (
 	"time"
 
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 var client *mongo.Client
 
-func New(c *mongo.Client) *Model {
+func New(c *mongo.Client) Models {
 	client = c
 	return Models{
 		LogEntry: LogEntry{},
@@ -78,4 +79,72 @@ func (l *LogEntry) All() ([]*LogEntry, error) {
 	}
 
 	return logs, nil
+}
+
+func (l *LogEntry) GetLogEntryByID(id string) (*LogEntry, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*15)
+	defer cancel()
+
+	collection := client.Database("logs").Collection("logs")
+
+	docID, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		fmt.Println("Error getting log id from MongoDB: ", err)
+		return nil, err
+	}
+
+	var log LogEntry
+	err = collection.FindOne(ctx, bson.M{"_id": docID}).Decode(&log)
+	if err != nil {
+		fmt.Println("Error getting log from MongoDB: ", err)
+		return nil, err
+	}
+
+	return &log, nil
+}
+
+func (l *LogEntry) Drop() error {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*15)
+	defer cancel()
+
+	collection := client.Database("logs").Collection("logs")
+
+	err := collection.Drop(ctx)
+	if err != nil {
+		fmt.Println("Error dropping logs from MongoDB: ", err)
+		return err
+	}
+
+	return nil
+}
+
+func (l *LogEntry) Update() (*mongo.UpdateResult, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*15)
+	defer cancel()
+
+	collection := client.Database("logs").Collection("logs")
+
+	docID, err := primitive.ObjectIDFromHex(l.ID)
+	if err != nil {
+		fmt.Println("Error getting log id from MongoDB: ", err)
+		return nil, err
+	}
+
+	result, err := collection.UpdateOne(
+		ctx,
+		bson.M{"_id": docID},
+		bson.D{
+			{Key: "$set", Value: bson.D{
+				{Key: "name", Value: l.Name},
+				{Key: "data", Value: l.Data},
+				{Key: "updated_at", Value: time.Now()},
+			}},
+		},
+	)
+	if err != nil {
+		fmt.Println("Error updating log in MongoDB: ", err)
+		return nil, err
+	}
+
+	return result, nil
 }
