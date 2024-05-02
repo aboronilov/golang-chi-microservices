252d5ac91bd8ffd712d55d7b5f91f3e87b186659
Anatoly Boronilov <boronilov@gmail.com>
2024-05-02 13:27:02 +0200

    listener service init, connect to RabbitMQ

---
+++ listener-service/event/consumer.go
@@ -0,0 +1,112 @@
+package event
+
+import (
+	"encoding/json"
+	"fmt"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+type Consumer struct {
+	conn      *amqp.Connection
+	queueName string
+}
+
+func NewConsumer(conn *amqp.Connection) (Consumer, error) {
+	consumer := Consumer{
+		conn: conn,
+	}
+
+	err := consumer.setUp()
+	if err != nil {
+		fmt.Println("Error setting up consumer: ", err)
+		return Consumer{}, err
+	}
+
+	return consumer, nil
+}
+
+func (c *Consumer) setUp() error {
+	channel, err := c.conn.Channel()
+	if err != nil {
+		fmt.Println("Error creating channel: ", err)
+		return err
+	}
+
+	return declareExchange(channel)
+}
+
+type Payload struct {
+	Name string `json:"name"`
+	Data string `json:"data"`
+}
+
+func (c *Consumer) Listen(topics []string) error {
+	ch, err := c.conn.Channel()
+	if err != nil {
+		fmt.Println("Error creating channel: ", err)
+		return err
+	}
+	defer ch.Close()
+
+	q, err := declareRandomQueue(ch)
+	if err != nil {
+		fmt.Println("Error declaring queue: ", err)
+		return err
+	}
+
+	for _, topic := range topics {
+		err = ch.QueueBind(
+			q.Name,
+			topic,
+			"logs_topic",
+			false,
+			nil,
+		)
+		if err != nil {
+			fmt.Println("Error binding queue: ", err)
+			return err
+		}
+	}
+
+	messages, err := ch.Consume(q.Name, "", true, false, false, false, nil)
+
+	forever := make(chan bool)
+	go func() {
+		for d := range messages {
+			var payload Payload
+			_ = json.Unmarshal(d.Body, &payload)
+
+			go handlePayload(payload)
+		}
+		forever <- true
+		return
+	}()
+
+	fmt.Printf("Waiting for message [Exchange, Queue] [logs_topic, %s]\n", q.Name)
+	<-forever
+
+	return nil
+}
+
+func handlePayload(payload Payload) {
+	switch payload.Name {
+	case "log", "event":
+		err := logEvent(payload)
+		if err != nil {
+			fmt.Println("Error logging event: ", err)
+		}
+	case "auth":
+		fmt.Printf("Unknown: %s\n", payload.Data)
+	default:
+		err := logEvent(payload)
+		if err != nil {
+			fmt.Println("Error logging event: ", err)
+		}
+	}
+}
+
+func logEvent(payload Payload) error {
+	fmt.Printf("%s: %s\n", payload.Name, payload.Data)
+	return nil
+}
---
+++ listener-service/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"math"
+	"os"
+	"time"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func main() {
+	// connect to RMQ
+	rabbitConn, err := connect()
+	if err != nil {
+		log.Println(err)
+		os.Exit(1)
+	}
+	defer rabbitConn.Close()
+	fmt.Println("Connected to RabbitMQ")
+
+	// start listening to messages
+	// create a consumer
+	// watch the queue and consume events
+}
+
+func connect() (*amqp.Connection, error) {
+	var counts int64
+	var backOff = 1 * time.Second
+	var connection *amqp.Connection
+
+	for {
+		c, err := amqp.Dial("amqp://guest:guest@localhost:5672")
+		if err != nil {
+			fmt.Println("RabbitMQ is not ready yet")
+			counts++
+		} else {
+			connection = c
+			break
+		}
+
+		if counts > 5 {
+			fmt.Println(err)
+			return nil, err
+		}
+
+		backOff = time.Duration(math.Pow(float64(counts), 2)) * time.Second
+		log.Println("Backing off...")
+		time.Sleep(backOff)
+		continue
+	}
+	return connection, nil
+}
